6c4b2e0412f9017688adc45a1c51ad205bb46d3e
Nicholas Haught <146685642+NickHaught@users.noreply.github.com>
2024-02-08 01:24:08 -0500

    Added pdf_to_img

---
+++ pdf_to_img.py
@@ -0,0 +1,30 @@
+import pypdfium2 as pdfium
+from PIL import Image
+from pathlib import Path
+
+current_directory = Path.cwd()
+
+pdf_directory = current_directory / 'pdf'
+image_directory = current_directory / 'images'
+
+print(pdf_directory)
+for entry in pdf_directory.iterdir():
+    print(entry)
+    pdf = pdfium.PdfDocument(entry)
+
+    # Access the first page (index 0)
+    print(f"Current {entry.name}")
+    page = pdf.get_page(int(input("Enter page: ")))
+
+    # Render the page to a bitmap
+    bitmap = pdfium.PdfPage.render(page, scale = 4)
+
+    # Convert the bitmap to a PIL image
+    pil_image = Image.frombytes("RGB", (bitmap.width, bitmap.height), bitmap.buffer)
+
+   # Save the PIL image in the 'images' folder
+    image_path = image_directory  / f"{entry.stem}_order.jpg"
+    pil_image.save(image_path)
+
+    # Close the PDF document
+    pdf.close()
