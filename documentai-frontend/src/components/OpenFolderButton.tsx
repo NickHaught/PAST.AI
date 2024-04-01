427765cf606459f118d44c6878f3cb7163d75585
NickHaught <nicholas.haught@outlook.com>
2024-03-31 23:20:29 -0400

    Added new upload button

--- documentai-frontend/src/components/OpenFolderButton.tsx
+++ documentai-frontend/src/components/OpenFolderButton.tsx
@@ -8,6 +8,7 @@ interface Props {
   onUploadComplete: (fileNames: string[]) => void;
 }
 
+<<<<<<< HEAD
 interface CustomInputAttributes extends React.HTMLProps<HTMLInputElement> {
   webkitdirectory?: string;
 }
@@ -15,10 +16,14 @@ interface CustomInputAttributes extends React.HTMLProps<HTMLInputElement> {
 // ! Issue when canceling an upload, current directory is reset
 
 const OpenFolderButton = ({ onUploadComplete }: Props) => {
+=======
+const OpenFolderButton = ({ className }: Props) => {
+>>>>>>> 80d94424b2e4be6569d26a21b70e88328690897c
   const { setFiles } = useFiles();
   const [uploadStatus, setUploadStatus] = useState<string | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
+<<<<<<< HEAD
   const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     const formData = new FormData();
@@ -45,11 +50,19 @@ const OpenFolderButton = ({ onUploadComplete }: Props) => {
       const pdfFiles = Array.from(files);
       console.log(pdfFiles);
       setFiles(pdfFiles);
+=======
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const files = Array.from(event.target.files ?? []);
+    const pdfFiles = files.filter((file) => file.type === "application/pdf");
+>>>>>>> 80d94424b2e4be6569d26a21b70e88328690897c
 
       try {
         const response = await uploadFiles(pdfFiles);
         if (response.status === 200) {
           setUploadStatus("Files uploaded successfully.");
+<<<<<<< HEAD
           onUploadComplete(response.data.pdfNames);
         } else {
           setUploadStatus("Upload failed with status: " + response.status);
@@ -61,13 +74,44 @@ const OpenFolderButton = ({ onUploadComplete }: Props) => {
     } else {
       console.error("No files selected.");
       setUploadStatus("No files were selected. Please select files to upload.");
+=======
+        } else {
+          // Handle any responses that didn't throw an error but aren't success status
+          setUploadStatus("Upload completed with unexpected status.");
+        }
+      } catch (error) {
+        if (axios.isAxiosError(error) && error.response) {
+          // Check if it's an AxiosError and has a response
+          if (error.response.status === 400) {
+            setUploadStatus(
+              "Bad request. Please check the files and try again."
+            );
+          } else {
+            // Handle other errors
+            setUploadStatus(`An error occurred: ${error.response.status}`);
+          }
+        } else {
+          // Error is not from Axios or doesn't have a response
+          setUploadStatus("An unexpected error occurred.");
+        }
+      }
+    } else {
+      console.error("No PDF files selected.");
+      setUploadStatus(
+        "No PDF files were selected. Please select PDF files to upload."
+      );
+>>>>>>> 80d94424b2e4be6569d26a21b70e88328690897c
     }
   };
 
   return (
     <>
       <button
+<<<<<<< HEAD
         className=" text-white flex items-center space-x-2 bg-light-gray hover:bg-light-gray text-sm py-1 px-2 rounded-lg mr-2 focus:outline-none hover:border-blue transition duration-150 ease-in-out"
+=======
+        className={`primary-text flex items-center space-x-2 bg-hover-gray hover:bg-hover-gray text-sm py-1 px-2 rounded-lg focus:outline-none hover:border-blue transition duration-150 ease-in-out ${className}`}
+>>>>>>> 80d94424b2e4be6569d26a21b70e88328690897c
         onClick={() => fileInputRef.current?.click()}
       >
         <FaArrowAltCircleUp />
