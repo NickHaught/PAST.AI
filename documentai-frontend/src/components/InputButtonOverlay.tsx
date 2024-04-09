63a86d84ca9b77592ac17309ad1e342fef0769c4
Grant Howard <grantmhoward4@gmail.com>
2024-04-09 14:50:24 -0400

    Prep for main merge

--- documentai-frontend/src/components/InputButtonOverlay.tsx
+++
@@ -1,8 +0,0 @@
-import HelloWorld from "../functions/HelloWorld";
-import OpenFileButton from "./OpenFolderButton";
-
-const InputButtonOverlay = () => {
-  return <OpenFileButton onClick={() => HelloWorld()} />;
-};
-
-export default InputButtonOverlay;
--- documentai-frontend/src/components/OutputButtonOverlay.tsx
+++
@@ -1,8 +0,0 @@
-import HelloWorld from "../functions/HelloWorld";
-import OpenFileButton from "./OpenFolderButton";
-
-const OutputButtonOverlay = () => {
-  return <OpenFileButton onClick={() => HelloWorld()} />;
-};
-
-export default OutputButtonOverlay;
