1143fb7825beeb2da8948c5b871881df0df07708
Nicholas Haught <146685642+NickHaught@users.noreply.github.com>
2024-04-09 14:59:58 -0400

    Delete documentai-backend directory

--- documentai-backend/SSDjango/teststudio/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class PDF(models.Model):
-    title = models.CharField(max_length=255)
-    file = models.FileField(upload_to='pdfs/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title or self.pdf_file.name
